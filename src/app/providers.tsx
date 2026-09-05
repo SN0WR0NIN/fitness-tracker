1e757a163c52f35d4920c6f20b6a6309b7d4a4cf
SN0WR0NIN <Ebby_taufiq@outlook.com>
2026-09-05 20:54:02 +0800

    Mount web vitals reporter

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,14 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import PwaManager from '@/components/PwaManager';
+import WebVitalsReporter from '@/components/WebVitalsReporter';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <PwaManager>{children}</PwaManager>
+        <WebVitalsReporter />
       </ThemeProvider>
     </SessionProvider>
   );
